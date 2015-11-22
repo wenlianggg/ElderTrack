957f71c08753e915c1f24accba023859f5ce77e3
Wen Liang Goh <wenlianggg@gmail.com>
2015-11-23 01:47:28 +0800

    Updates to weather information

---
+++ src/eldertrack/weather/Weather.java
@@ -0,0 +1,11 @@
+package eldertrack.weather;
+
+import java.util.List;
+
+class Weather {
+    double latitude;
+    double longitude;
+    String timezone;
+    int offset;
+    List<String> currentweather;
+}
