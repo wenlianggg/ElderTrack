6276e6fd2756655d1316de24a22c91abceeebcd4
T3hcamp3r <153010N@mymail.Nyp.edu.sg>
2015-11-23 19:06:59 +0800

    Added table to database for elderly, created DisplayElderInfo.java

---
+++ src/eldertrack/management/DisplayElderInfo.java
@@ -0,0 +1,12 @@
+package eldertrack.management;
+
+import java.sql.*;
+
+import eldertrack.db.SQLConnect;
+
+
+public class DisplayElderInfo {
+	public static void main(String[] args) throws SQLException {
+		ResultSet rs = SQLConnect.getResultSet("SELECT * FROM et_elderly");
+	}
+}
