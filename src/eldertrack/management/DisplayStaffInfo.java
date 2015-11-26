19e0b26d5b9d6210d972edcd29f9d5c231b9a3c1
froststein <155125y@mymail.nyp.edu.sg>
2015-11-26 09:33:25 +0800

    Show staff added

---
+++ src/eldertrack/management/DisplayStaffInfo.java
@@ -0,0 +1,17 @@
+package eldertrack.management;
+import java.sql.*;
+
+import eldertrack.db.SQLConnect;
+
+public class DisplayStaffInfo {
+public static void main(String[] args) throws SQLException {
+		
+		ResultSet rs = SQLConnect.getResultSet("SELECT * FROM et_staff");
+		
+		while(rs.next()){
+		System.out.println("Name: "+rs.getString("firstname"));
+		
+		System.out.println("");
+	}
+}
+}
