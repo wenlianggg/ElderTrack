2225e29a46472162ec3db57af5e0fc9db70c4f79
Wen Liang Goh <wenlianggg@gmail.com>
2015-12-21 21:35:03 +0800

    Commented out non-working/unused parts of code, please fix.

--- src/eldertrack/ui/ReportModifyPanel.java
+++ src/eldertrack/ui/ReportModifyPanel.java
@@ -11,11 +11,8 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextField;
+
 import javax.swing.border.LineBorder;
-import javax.swing.table.DefaultTableModel;
 import javax.swing.JTextArea;
 
 public class ReportModifyPanel extends JPanel {
