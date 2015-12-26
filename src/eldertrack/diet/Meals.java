1145d627e029c622f5386c44627a84dcfe4cdd5b
Wen Liang Goh <wenlianggg@gmail.com>
2015-12-27 01:28:02 +0800

    Add meals serializable class.

---
+++ src/eldertrack/diet/Meals.java
@@ -0,0 +1,10 @@
+package eldertrack.diet;
+
+import java.io.Serializable;
+
+public class Meals implements Serializable {
+	private static final long serialVersionUID = 1000L;
+	public Meals() {
+		// TODO Auto-generated constructor stub
+	}
+}
