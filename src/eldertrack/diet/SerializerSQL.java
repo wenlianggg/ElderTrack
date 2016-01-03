3283d454c3b32086fa3a1a64f5669bfc0ce29604
Wen Liang Goh <wenlianggg@gmail.com>
2016-01-03 11:14:08 +0800

    Create retriever and store (SerializerSQL)

---
+++ src/eldertrack/diet/SerializerSQL.java
@@ -0,0 +1,53 @@
+package eldertrack.diet;
+
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInputStream;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import eldertrack.db.SQLObject;
+
+public class SerializerSQL {
+	static final String SQL_WRITE = "UPDATE et_elderly SET diet = ? WHERE id = ?";
+	static final String SQL_READ = "SELECT diet FROM et_elderly WHERE id = ?";
+	private static SQLObject so = new SQLObject();
+    private ObjectInputStream is;
+	public boolean store(int id, Meals m) {
+			  PreparedStatement ps;
+			  try {
+				  ps = so.getPreparedStatementWithKey(SQL_WRITE);
+				  ps.setObject(1, m);
+				  ps.setInt(2, id);
+				  ps.executeUpdate();
+				  ps.close();
+			  } catch (SQLException e) {
+				  e.printStackTrace();
+				  return false;
+			  }	catch (Exception e) {
+				  e.printStackTrace();
+				  return false;
+			  }
+			  return true;
+	}
+
+	public Meals retrieve(int id) {
+	    PreparedStatement ps;
+	    Meals m;
+	    try {
+		    ps = so.getPreparedStatementWithKey(SQL_READ);
+			ps.setInt(1, 1);
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+		    is = new ObjectInputStream(new ByteArrayInputStream(rs.getBytes(1)));
+			m = (Meals) is.readObject();
+	    } catch (SQLException e) {
+	    	e.printStackTrace();
+	    	return null;
+	    } catch (Exception e) {
+	    	e.printStackTrace();
+	    	return null;
+	    }
+		return m;
+	}
+}
