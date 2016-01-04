1d40e81b25893165a36ef7fc184f91f3d8457aeb
Wen Liang Goh <wenlianggg@gmail.com>
2016-01-05 02:22:08 +0800

    Removed unused imports and remove deprecated code reference

--- src/eldertrack/management/DisplayStaffInfo.java
+++ src/eldertrack/management/DisplayStaffInfo.java
@@ -1,12 +1,12 @@
 package eldertrack.management;
 import java.sql.*;
 
-import eldertrack.db.SQLConnect;
+import eldertrack.db.SQLObject;
 
 public class DisplayStaffInfo {
 public static void main(String[] args) throws SQLException {
-		
-		ResultSet rs = SQLConnect.getResultSet("SELECT * FROM et_staff");
+		SQLObject so = new SQLObject();
+		ResultSet rs = so.getResultSet("SELECT * FROM et_staff");
 		
 		while(rs.next()){
 		System.out.println("Name: "+rs.getString("firstname")+" " +rs.getString("lastname") );
