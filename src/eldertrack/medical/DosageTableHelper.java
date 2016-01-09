b63afade4e169c4f10a465a300420a380baa0f60
froststein <155125y@mymail.nyp.edu.sg>
2016-01-09 23:17:03 +0800

    It displays everything now

--- src/eldertrack/medical/DosageTableHelper.java
+++ src/eldertrack/medical/DosageTableHelper.java
@@ -19,24 +19,24 @@ import javax.swing.table.DefaultTableModel;
 import eldertrack.db.SQLObject;
 
 public class DosageTableHelper {
-	 static final SQLObject so = new SQLObject();
-	 
-	  public static DefaultTableModel getElderlyFromQueryDos(String search) throws SQLException {
-			search = (search.equalsIgnoreCase("")) ? "%" : search;
-			return buildTableModel(so.getResultSet("SELECT medication FROM et_elderly WHERE name LIKE ?", search));
-		}
-		// Method from http://stackoverflow.com/questions/10620448/most-simple-code-to-populate-jtable-from-resultset
-	  
-		@SuppressWarnings("unchecked")
-		public static DefaultTableModel buildTableModel(ResultSet rs) throws SQLException {
-			ArrayList<DosageObject> retrieveDosBlob = null;
-			ArrayList<ArrayList> storeList=new ArrayList<ArrayList>();
-			int counter=1;
-			try {
-				while(rs.next()){
+	static final SQLObject so = new SQLObject();
+
+	public static DefaultTableModel getElderlyFromQueryDos(String search) throws SQLException {
+		search = (search.equalsIgnoreCase("")) ? "%" : search;
+		return buildTableModel(so.getResultSet("SELECT medication FROM et_elderly WHERE name LIKE ?", search));
+	}
+	// Method from http://stackoverflow.com/questions/10620448/most-simple-code-to-populate-jtable-from-resultset
+
+	@SuppressWarnings("unchecked")
+	public static DefaultTableModel buildTableModel(ResultSet rs) throws SQLException {
+		ArrayList<DosageObject> retrieveDosBlob = null;
+		ArrayList<ArrayList> storeList=new ArrayList<ArrayList>();
+		int counter=1;
+		try {
+			while(rs.next()){
 				ResultSetMetaData metaData = rs.getMetaData();
-			    System.out.println(metaData.getColumnCount());
-			    PreparedStatement statement = so.getPreparedStatementWithKey("SELECT medication FROM et_elderly WHERE id = ?");
+				System.out.println(metaData.getColumnCount());
+				PreparedStatement statement = so.getPreparedStatementWithKey("SELECT medication FROM et_elderly WHERE id = ?");
 				statement.setInt(1, counter);
 				ResultSet rs1 = statement.executeQuery();
 				rs1.next();
@@ -46,69 +46,67 @@ public class DosageTableHelper {
 				retrieveDosBlob = (ArrayList<DosageObject>) is.readObject();
 				storeList.add(retrieveDosBlob);
 				counter++;
-				}
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// storing array list in an array list for future uses
+
+		Vector<String> columnNames = new Vector<String>();
+
+		columnNames.add("Description");
+		columnNames.add("Prescription");
+		columnNames.add("Medication Type");
+		columnNames.add("Dosage");
+		columnNames.add("Checked");
+		Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+		// database print all IDs
+		// vector due to table
+
+		
+
+		ArrayList<DosageObject> nowList=storeList.get(0);
+
+		for(int k=0;k<nowList.size();k++){
+			DosageObject dosing=nowList.get(k);
+			Vector<Object> vector = new Vector<Object>();
+			vector.add(dosing.getMedDescrip());
+			vector.add(dosing.getMedPrescrip());
+			vector.add(dosing.getMedType());
+			vector.add(dosing.getMedDosage());
+			vector.add(Boolean.FALSE);
+			data.add(vector);
 			
-			// storing array list in an array list for future uses
-			for(int i=0;i<storeList.size();i++){
-				ArrayList<DosageObject> nowList=storeList.get(i);
-				System.out.println(i);
-				for(int k=0;k<nowList.size();k++){
-					nowList.get(k).print();
-				}
-			}
-		    Vector<String> columnNames = new Vector<String>();
-		    
-		    columnNames.add("Description");
-		    columnNames.add("Prescription");
-		    columnNames.add("Medication Type");
-		    columnNames.add("Dosage");
-		    columnNames.add("Checked");
-		    Vector<Vector<Object>> data = new Vector<Vector<Object>>();
-		    // database print all IDs
-		    // vector due to table
-		   
-		        Vector<Object> vector = new Vector<Object>();
-		        
-		        for (int columnIndex = 1; columnIndex <= 5; columnIndex++) {
-		        	for(int i=0;i<retrieveDosBlob.size();i++){
-			            vector.add(retrieveDosBlob.get(i).getMedDescrip());
-			            vector.add(retrieveDosBlob.get(i).getMedPrescrip());
-			            vector.add(retrieveDosBlob.get(i).getMedType());
-			            vector.add(retrieveDosBlob.get(i).getMedDosage());
-			            vector.add(Boolean.FALSE);
-			            
-		        	}
-		        	
-		        }
-		        data.add(vector);
-		   
-		    DefaultTableModel dtm = new DefaultTableModel(data, columnNames) {
-				private static final long serialVersionUID = 4234183862785566645L;
-				@Override
-		        public boolean isCellEditable(int row, int column) {
-		           return false;
-		        }
-		    };
-		    return dtm;
 		}
 		
-		// Debug-able main method
-		public static void main(String[] args) throws SQLException {
-		    JTable toDoTable = new JTable(getElderlyFromQueryDos(""));
-		    JScrollPane jpane = new JScrollPane(toDoTable);
-		    JPanel panel = new JPanel();
-		    JFrame frame = new JFrame();
-		    frame.setBounds(0, 0, 700, 500);
-		    panel.add(jpane);
-		    frame.getContentPane().add(new JScrollPane(panel));
-		    frame.setVisible(true);
-		   
-		}
+
+
+
+		DefaultTableModel dtm = new DefaultTableModel(data, columnNames) {
+			private static final long serialVersionUID = 4234183862785566645L;
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
+		return dtm;
+	}
+
+	// Debug-able main method
+	public static void main(String[] args) throws SQLException {
+		JTable toDoTable = new JTable(getElderlyFromQueryDos(""));
+		JScrollPane jpane = new JScrollPane(toDoTable);
+		JPanel panel = new JPanel();
+		JFrame frame = new JFrame();
+		frame.setBounds(0, 0, 700, 500);
+		panel.add(jpane);
+		frame.getContentPane().add(new JScrollPane(panel));
+		frame.setVisible(true);
+
+	}
 }
