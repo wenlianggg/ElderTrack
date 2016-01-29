3e40339dcf9879ff16ee90078daafc5ede1cf234
Wen Liang - Laptop <wenlianggg@gmail.com>
2016-01-29 09:47:35 +0800

    Changes

--- src/eldertrack/ui/MarqueePanel.java
+++ src/eldertrack/ui/MarqueePanel.java
@@ -39,6 +39,28 @@ public class MarqueePanel extends JPanel implements ActionListener {
         label.setText(sb.toString());
         this.add(label);
     }
+    
+    public MarqueePanel(String s, int n, Font font) {
+    	if (s.equals("")) {
+
+    	}
+        if (s == null || n < 1) {
+            throw new IllegalArgumentException("Null string or n < 1");
+        }
+        StringBuilder sb = new StringBuilder(n);
+        for (int i = 0; i < n; i++) {
+            sb.append(' ');
+        }
+        this.s = sb + s + sb;
+        this.n = n;
+        label.setFont(font);
+        label.setForeground(Color.WHITE);
+        label.setText(sb.toString());
+        this.add(label);
+    }
+    
+    
+    
 
     public void start() {
         timer.start();
